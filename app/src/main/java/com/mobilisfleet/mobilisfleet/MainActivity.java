bb42b901ceb576c04d2883250238e77e70c035ee
Daniel Bacha <dcbacha@gmail.com>
2017-06-27 00:31:29 -0300

    webView

--- app/src/main/java/com/mobilisfleet/mobilisfleet/MainActivity.java
+++ app/src/main/java/com/mobilisfleet/mobilisfleet/MainActivity.java
@@ -4,30 +4,32 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.webkit.WebSettings;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
+    private WebView mWebView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-      //  WebView myWebView = (WebView) findViewById(R.id.webview);
-      //  WebSettings webSettings = myWebView.getSettings();
-      //  webSettings.setJavaScriptEnabled(true);
-      //  myWebView.loadUrl("https://mobilis.eco.br");
-
-     //   WebView webview = new WebView(this);
-     //   setContentView(webview);
-     //   webview.loadUrl("https://mobilis.eco.br");
-
-       // Intent browserIntent = new Intent("android.intent.action.VIEW", Uri.parse("https://mobilis.eco.br"));
-       // startActivity(browserIntent);
-
-        Uri uri = Uri.parse("https://mobilis.eco.br");
-        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-        startActivity(intent);
+        String URL = "https://mobilis.eco.br";
+        mWebView = (WebView) this.findViewById(R.id.webView);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.loadUrl(URL);
+        mWebView.setWebViewClient(new WebViewClient());
     }
+
+    @Override
+    public void onBackPressed() {
+        if(mWebView.canGoBack()){
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
 }
